5fb2159a68542d4bebb95c7f0e4230c8a2510a90
nethubot222 <115031240+nethubot222@users.noreply.github.com>
2022-10-09 20:36:36 -0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -12,9 +12,9 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['94784506970']
-global.premium = ['94784506970']
-global.ownernomer = '94784506970'
+global.owner = ['94781789988']
+global.premium = ['94781789988
+global.ownernomer = '94781789988
 global.ownername = 'Achi Fernando'
 global.botname = '🎭ᴀʟᴘʜᴀ ᴡʜᴀᴛꜱᴀᴘᴘ ʙᴏᴛ-ᴍᴅ ᴇᴅɪᴛɪᴏɴ🎭 v1.0.0'
 global.footer = ' ᴀʟᴘʜᴀ ʙᴏᴛ-ᴍᴅ ᴇᴅɪᴛɪᴏɴ'
